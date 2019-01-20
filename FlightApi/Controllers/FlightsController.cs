0efadf8d1d448ae17cf6cf11a0bb16ddb8f28f6b
Mikkel Steenberg Kristoffersen <mikk6053@edu.eal.dk>
2019-01-20 19:22:32 +0100

    Add project files.

---
+++ FlightApi/Controllers/FlightsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FlightApi.FlightDataContext;
+using FlightApi.Models;
+
+namespace FlightApi.Controllers
+{
+    [Route("api/Flights")] //Husk at tjekke properties LaunchSettings
+    [ApiController]
+    //[Produces("aplication/xml")] tvinger alt til at blive vist som XML
+    public class FlightsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public FlightsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Flights
+        [HttpGet]
+        public IEnumerable<Flight> GetFlights()
+        {
+            return _context.Flights;
+        }
+
+        // GET: api/Flights/5
+        [HttpGet("AllFlights")]
+        public ActionResult<IEnumerable<Flight>> AllFlights()
+        {
+            List<Flight> flights = _context.Flights.ToList();
+            if (flights == null)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid) //serverside validation. Hvis du i dit model lag har tilføjet fx annotaion [MinLenght(100)] så checker ModelState op om længden bliver overholdt
+            {
+                return BadRequest();
+            }
+
+            return flights;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Flight> Get(int id)
+        {
+            var flight = _context.Flights.First(x => x.FlightId == id);
+            return flight;
+        }
+
+        [HttpGet("SpecificFlights/{FromLocation}/{ToLocation}")]
+        public List<Flight> GetSpecificFlights(string FromLocation, string ToLocation) 
+        {
+            List<Flight> specificFlights = new List<Flight>();
+            foreach (var item in _context.Flights)
+            {
+                if (item.FromLocation == FromLocation && item.ToLocation == ToLocation)
+                {
+                    specificFlights.Add(item);
+                }
+            }
+            return specificFlights;
+        }
+
+        [HttpGet("alldeparturesandarrivals")]
+        public List<Flight> AllDeparturesAndArrivals()
+        {
+            List<Flight> flights = _context.Flights.ToList();
+            return flights;
+        }
+
+        [HttpPut("{id}")]
+        public void Put([FromBody] Flight flight)
+        {
+            //flight.FlightId = id;
+            var saved = false;
+            while (!saved)
+            {
+                try
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        BadRequest(ModelState);
+                    }
+                    _context.Flights.Update(flight);
+                    _context.SaveChanges();
+                    saved = true;
+                }
+                catch (DbUpdateConcurrencyException ex) //checker for cuncurrency
+                {
+                    ex.Entries.Single().Reload(); //Database værdier bliver IKKE overskrevet
+                    _context.SaveChanges();
+                    saved = true;
+                }
+            }
+        }
+
+        // POST: api/Flights
+        [HttpPost]
+        public void PostFlight([FromBody] Flight flight)
+        {
+            if (!ModelState.IsValid)
+            {
+                BadRequest(ModelState);
+            }
+
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+            
+        }
+
+        // DELETE: api/Flights/5
+        [HttpDelete("deleteflight/{id}")]
+        public async Task<IActionResult> DeleteFlight([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var flight = await _context.Flights.FindAsync(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            _context.Flights.Remove(flight);
+            await _context.SaveChangesAsync();
+
+            return Ok(flight);
+        }
+
+        private bool FlightExists(int id)
+        {
+            return _context.Flights.Any(e => e.FlightId == id);
+        }
+    }
+}
---
+++ FlightMvc/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FlightMvc.Models;
+using System.Net.Http;
+
+namespace FlightMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string API = "https://localhost:44329";
+        public static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(API);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = GetClient();
+            HttpResponseMessage response = await client.GetAsync($@"api/Flights/AllFlights");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = await response.Content.ReadAsAsync<List<FlightProxy>>();
+
+                return View(model);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public IActionResult SearchFlights()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> EditFlights(int id)
+        {
+            var client = GetClient();
+            HttpResponseMessage response = await client.GetAsync($@"api/Flights/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = await response.Content.ReadAsAsync<FlightProxy>();
+
+                return View(model);
+            }
+            else
+            {
+                return BadRequest("An error occured.");
+            }
+        }
+
+        public async Task<IActionResult> EditFlightsPut(FlightProxy flight)
+        {
+            var client = GetClient();
+            HttpResponseMessage response = await client.PutAsJsonAsync($@"api/airport/{flight.FlightId}", flight);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = await response.Content.ReadAsAsync<FlightProxy>();
+
+                return RedirectToAction("Index");
+                //return View(model);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public async Task<IActionResult> SearchFlightsAction(string FromLocation, string ToLocation)
+        {
+            var client = GetClient();
+            HttpResponseMessage response = await client.GetAsync($@"api/Flights/SpecificFlights/{FromLocation}/{ToLocation}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = await response.Content.ReadAsAsync<List<FlightProxy>>();
+
+                return View(model);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public async Task<IActionResult> SeeAllLocations()
+        {
+            var client = GetClient();
+            HttpResponseMessage response = await client.GetAsync($@"api/Flights/AllDeparturesAndArrivals");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = await response.Content.ReadAsAsync<List<FlightProxy>>();
+
+                return View(model);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public async Task<IActionResult> DeleteFlight(int id)
+        {
+            var client = GetClient();
+            string request = $@"api/Flights/deleteflight/{id}";
+            HttpResponseMessage response = await client.DeleteAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
